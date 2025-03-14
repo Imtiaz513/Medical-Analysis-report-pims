044da2889b12f7bb9d7881647ee8914ab792dfac
Imtiaz Hussain <ihsofh@gmail.com>
2025-03-14 19:49:20 +0500

    Update script_R.R

--- script_R.R
+++ script_R.R
@@ -51,7 +51,7 @@ data <- data %>%
 str(data)
 library(dplyr)
 
-## Rename variables with shorter names
+#### Rename variables with shorter names
 your_dataframe <- your_dataframe %>% rename(
   Subject_ID = `Subject ID`,
   Age = `How old are you?`,
