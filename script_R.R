4ea7e8105715fdb5baa4b2b63c2a1b99f3b4e612
Imtiaz Hussain <ihsofh@gmail.com>
2025-03-14 19:47:37 +0500

    Update script_R.R

--- script_R.R
+++ script_R.R
@@ -5,7 +5,7 @@
  library(dplyr)
  library(forcats)
 
-## Convert specified columns to factors
+### Convert specified columns to factors
 data <- data %>%
   mutate(across(c(
     `How old are you?`, 
